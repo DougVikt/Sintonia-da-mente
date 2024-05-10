f2668352daee6dd2f7a99d462192d21ea8e09347
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-05-10 14:31:31 +0000

    add bootstrap

--- sintonia-mental/src/index.js
+++ sintonia-mental/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import NavBar from './navbar/navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
