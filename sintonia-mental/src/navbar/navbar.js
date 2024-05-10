1d9bf6c480a4171ca9c889add7956e1975bdb10b
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-05-10 14:19:52 +0000

    new projeto

---
+++ sintonia-mental/src/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import buton from "react-bootstrap/"
+
+function NavBar(){
+    return (
+    <>
+    <h1> teste </h1>
+    <a bg="danger" as={buton}>botao</a>
+    </>
+    )
+}
+
+export default NavBar;
