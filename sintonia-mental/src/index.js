0042d0724c7905068abc7e094474c08edbcacc59
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-05-10 14:53:32 +0000

    add

--- sintonia-mental/src/index.js
+++ sintonia-mental/src/index.js
@@ -6,6 +6,7 @@ import NavBar from './navbar/navbar';
 
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- sintonia-mental/src/navbar/navbar.js
+++ sintonia-mental/src/navbar/navbar.js
@@ -1,11 +1,10 @@
 import React from "react";
-import buton from "react-bootstrap/"
 
 function NavBar(){
     return (
     <>
     <h1> teste </h1>
-    <a bg="danger" as={buton}>botao</a>
+    <a className="btn bg-danger">botao</a>
     </>
     )
 }
