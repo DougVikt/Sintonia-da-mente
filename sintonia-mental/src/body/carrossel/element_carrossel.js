ad2896b3f83a1b84b880b8a3c3de7fd6adfc2f11
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-06-16 19:06:54 -0300

    add

--- sintonia-mental/src/body/carrossel/element_carrossel.js
+++ sintonia-mental/src/body/carrossel/element_carrossel.js
@@ -4,32 +4,30 @@ import imagem1 from '../../imagens/mente1.jpg';
 import imagem2 from '../../imagens/mente2.jpg';
 import imagem3 from '../../imagens/mente3.jpg';
 
-function ItemCarrossel(classe , imagem){
-    return(
-    <div className ={classe}>
-        <img src={imagem} className="d-block" alt="..."/>
-    </div>
-);}
+
 function Element_carrossel(){
+    
     return (
         <>
-            <div id="carrosselExampleSlidesOnly" className="carousel slide sty-carrossel mb-3" data-bs-ride="carousel">
+            <div id="carrosselExampleSlidesFade" className="carousel slide carousel-fade sty-carrossel mb-3" data-bs-ride="carousel" data-bs-interval="9000" data-bs-transition-duration="9">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <ItemCarrossel classe="sty-carrossel" imagem={imagem1}/>
-                    </div>
-                    <div className="carousel-item">
-                        <div className="sty-carrossel">
-                            <img src={imagem2} className="d-block" alt="..."/>
+                        <div className ="sty-carrossel">
+                            <img src={imagem1} className="d-block" alt="{alt}"/>
+                            <div>
+                                <h1>bemvindo</h1>
+                            </div>
                         </div>
                     </div>
                     <div className="carousel-item">
-                        <div className="sty-carrossel">
-                            <img src={imagem3} className="d-block" alt="..."/>
+                        <div className ="sty-carrossel">
+                            <img src={imagem2} className="" alt=""/>
+                            <div>
+                                <h1>bemvindo</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </>
     )
