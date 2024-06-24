8e40b8f5f0789cd28141cd23701f02afe1cd3510
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-06-23 21:23:39 -0300

    add

--- sintonia-mental/src/body/carrossel/element_carrossel.js
+++ sintonia-mental/src/body/carrossel/element_carrossel.js
@@ -9,27 +9,23 @@ function Element_carrossel(){
     
     return (
         <>
-            <div id="carrosselExampleSlidesFade" className="carousel slide carousel-fade sty-carrossel mb-3" data-bs-ride="carousel" data-bs-interval="9000" data-bs-transition-duration="9">
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <div className ="sty-carrossel">
-                            <img src={imagem1} className="d-block" alt="{alt}"/>
-                            <div>
-                                <h1>bemvindo</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <div className ="sty-carrossel">
-                            <img src={imagem2} className="" alt=""/>
-                            <div>
-                                <h1>bemvindo</h1>
-                            </div>
-                        </div>
+        <div id="carrosselExampleSlidesFade" className="carousel slide carousel-fade sty-carrossel mb-3" data-bs-ride="carousel" data-bs-interval="9000" data-bs-transition-duration="9">
+          <div className="carousel-inner d-flex">
+            <div className="carousel-item active ">
+              <div className="card row h-100 d-flex">
+                <div className="col-6 w-50 " >
+                    <img src={imagem1} alt="{alt}" className="img-fluid" />
+                  </div>
+                  <div className="col-6 flex-grow-1">
+                    <div className="card-body w-50 ">
+                      <h1>bemvindo</h1>
                     </div>
                 </div>
+              </div>
             </div>
-        </>
+          </div>
+        </div>
+      </>
     )
 }
 export default Element_carrossel;
