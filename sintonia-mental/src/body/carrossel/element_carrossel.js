f741dd171d7c0e556f3f82bcfb05eceb93cff937
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-06-25 21:37:40 -0300

    add

--- sintonia-mental/src/body/carrossel/element_carrossel.js
+++ sintonia-mental/src/body/carrossel/element_carrossel.js
@@ -10,16 +10,14 @@ function Element_carrossel(){
     return (
         <>
         <div id="carrosselExampleSlidesFade" className="carousel slide carousel-fade sty-carrossel mb-3" data-bs-ride="carousel" data-bs-interval="9000" data-bs-transition-duration="9">
-          <div className="carousel-inner d-flex">
-            <div className="carousel-item active ">
-              <div className="card row h-100 d-flex">
-                <div className="col-6 w-50 " >
-                    <img src={imagem1} alt="{alt}" className="img-fluid" />
-                  </div>
-                  <div className="col-6 flex-grow-1">
-                    <div className="card-body w-50 ">
-                      <h1>bemvindo</h1>
-                    </div>
+          <div className="carousel-inner d-flex flex-row container p-0 m-0">
+            <div className="carousel-item active w-100">
+              <div className="row p-0 ">
+                <div className="col-sm-6 " >
+                  <img src={imagem1} alt="{alt}" className="img-fluid w-100 " />
+                </div>
+                <div className="col-sm-6 ">
+                  <h1>bemvindo</h1>
                 </div>
               </div>
             </div>
