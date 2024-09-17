92a928ea6591c58d96c9aba5859f72b77781e932
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-09-17 18:29:47 -0300

    acionando as perguntas e finalizando as outras 2 paginas

--- SintoniaMental/questionario_app/urls.py
+++ SintoniaMental/questionario_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('questionario_app/', views.quest, name='quest'),
     path('questionario_app/quest_user', views.quest_user, name='quest_user'),
+    path('questionario_app/quest_pais' , views.quest_pais, name="quest_pais"),
+    path("questionario_app/quest_prof",views.quest_prof , name='quest_prof'),
 
     
 ]
--- SintoniaMental/questionario_app/views.py
+++ SintoniaMental/questionario_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .perg_alter import perguntas_usu , alternativas 
+from .perg_alter import * 
 
 
 
@@ -7,41 +7,23 @@ def quest(request):
     return render(request, 'questionario.html') 
 
 def quest_user(request):
-  
     context = {
         'perguntas': perguntas_usu,
         'alternativas': alternativas
     }
     return render(request, 'quest_usuario.html', context)
 
+def quest_pais(request):
+    context = {
+        'perguntas':perguntas_pais,
+        'alternativas':alternativas
+    }
+    return render(request,'quest_pais.html' , context)
 
 
-
-
-# from django.shortcuts import render, redirect
-# from .models import Question, Answer
-# from .forms import QuestionForm
-
-# def questionnaire(request):
-#     perguntas_usu = Question.objects.all()
-#     current_question = perguntas_usu.first()
-#     answers = []
-
-#     if request.method == 'POST':
-#         form = QuestionForm(request.POST)
-#         if form.is_valid():
-#             answer = form.save(commit=False)
-#             answer.question = current_question
-#             answer.save()
-#             answers.append(answer)
-#             current_question = current_question.get_next_question()
-#             if not current_question:
-#                 return redirect('questionnaire_results')
-#     else:
-#         form = QuestionForm()
-
-#     return render(request, 'questionnaire.html', {
-#         'form': form,
-#         'current_question': current_question,
-#         'answers': answers,
-#     })
+def quest_prof(request):
+    context = {
+        'perguntas':perguntas_prof,
+        'alternativas':alternativas
+    }
+    return render(request,'quest_prof.html' , context)
