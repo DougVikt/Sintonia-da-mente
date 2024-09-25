e7d6417caba4831377d7530166b4de48992ca04a
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-09-25 20:17:04 +0000

    editando o cadastro lite

--- SintoniaMental/cadastro_app/urls.py
+++ SintoniaMental/cadastro_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro_app', views.casd_lite , name='casd_lite'),
+    path('cadastro_app', views.cads_lite , name='cads_lite'),
 ]
--- SintoniaMental/cadastro_app/views.py
+++ SintoniaMental/cadastro_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-def casd_lite(request):
+def cads_lite(request):
     return render(request , 'simplificado.html')
 
