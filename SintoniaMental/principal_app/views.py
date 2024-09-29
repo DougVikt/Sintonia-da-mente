e2eedbe78f19363494f02e9501f4b89a23fbf5b9
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-09-29 20:55:03 -0300

    nodificando o nav para usuario

--- SintoniaMental/principal_app/views.py
+++ SintoniaMental/principal_app/views.py
@@ -1,22 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated :
+        nome_usuario = request.user.username
+    else :
+        nome_usuario = ""
+    return render(request, 'home.html', {'nome_usuario' :nome_usuario})
 
-# PARA PEGAR UM ID ALEATORIO E CRIAR UM CARD 
-
-# from django.shortcuts import render
-# from .models import Tabela (colocar o nome da tabela)
-# import random
-
-# def home(request):
-#     num_linhas = Tabela.objects.count()
-#     ids_aleatorios = random.sample(range(1, num_linhas + 1), 3)
-#     objetos = Tabela.objects.filter(id__in=ids_aleatorios)
-#       return render(request, 'home.html', {'objetos': objetos})
-
-#  UMA SEGUNDA MANEIRA
-
-#     objetos = Card.objects.all().order_by('?')[:3]
-#       return render(request, 'home.html', {'objetos': objetos})
