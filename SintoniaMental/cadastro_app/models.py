932a89449c886e58a746e5cc90e914cb13a83959
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-09-29 00:51:57 -0300

    retoques finais no cadastro simplificado

--- SintoniaMental/cadastro_app/models.py
+++ SintoniaMental/cadastro_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField( max_length=50)
+    email = models.EmailField(unique=True)
+    fone = models.CharField(max_length=15)
+    senha = models.CharField(max_length=128)
+    
+    def cripto_senha(self , text_senha):
+        self.senha = make_password(text_senha)
---
+++ SintoniaMental/static/Js/simplifiq_func.js
@@ -0,0 +1,32 @@
+function ValidacaoSenha(){
+    let senha = document.getElementById('senha');
+    let confereSenha = document.getElementById('confere-senha');
+    let msmErro = document.getElementById('erro');
+
+    if (senha.value !== confereSenha.value) {
+      senha.classList.add('is-invalid');
+      confereSenha.classList.add('is-invalid');
+      msmErro.innerHTML= 'As senhas não são identicas !'
+      
+      return false;
+    }
+    
+    return true;
+   
+  }
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var telefoneInput = document.getElementById('fone');
+
+    telefoneInput.addEventListener('input', function (e) {
+        var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,1})(\d{0,4})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : '(' + x[1] + ') 9' + x[3] + (x[4] ? '-' + x[4] : '');
+    });
+
+    telefoneInput.addEventListener('keydown', function (e) {
+        if (!/[0-9]/.test(e.key) && !['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight', 'Tab'].includes(e.key)) {
+            e.preventDefault();
+        }
+    });
+  });
