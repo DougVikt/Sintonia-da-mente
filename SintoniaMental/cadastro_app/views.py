9346ce95069b042712410424f559f6a73f10660f
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-10-01 19:17:51 +0000

    testando o cadastro , erro por ser no vs online

--- SintoniaMental/cadastro_app/views.py
+++ SintoniaMental/cadastro_app/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import Usuario
 
 def cads_lite(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        fone = request.POST.get('fone')
+        senha =  request.POST.get('senha')
+        usuario = Usuario(
+            nome=nome, email=email, fone=fone )
+        usuario.cripto_senha(senha)
+        usuario.save()
+        return redirect('home')
+        
+    
     return render(request , 'usuario/simplificado.html')
 
