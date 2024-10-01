4de99da4d7599e2e18035736613f121240749259
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-10-01 21:30:06 +0000

    adicionando o cadastro simples do profissional

--- SintoniaMental/cadastro_app/urls.py
+++ SintoniaMental/cadastro_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro_app/usuario', views.cads_lite , name='cads_lite'),
+    path('cadastro_app/usuario', views.cads_lite_user , name='cads_lite_user'),
+    path('cadastro_app/profissional', views.cads_lite_prof , name='cads_lite_prof'),
 ]
--- SintoniaMental/cadastro_app/views.py
+++ SintoniaMental/cadastro_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from .models import Usuario
 
-def cads_lite(request):
+def cads_lite_user(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
         email = request.POST.get('email')
@@ -11,8 +11,11 @@ def cads_lite(request):
             nome=nome, email=email, fone=fone )
         usuario.cripto_senha(senha)
         usuario.save()
-        return redirect('home')
+        return redirect('usuario/simplificado.html')
         
     
     return render(request , 'usuario/simplificado.html')
 
+
+def cads_lite_prof(requst):
+    return render(requst , 'profissional/simplificado.html')
