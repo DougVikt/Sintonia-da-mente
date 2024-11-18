459bdee523b833509fae135fe70d5bc933ab4fff
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-11-18 18:56:52 +0000

    algumas anotações

--- SintoniaMental/SintoniaMental/__init__.py
+++ SintoniaMental/SintoniaMental/__init__.py
@@ -20,3 +20,20 @@
 #  comando para criar o banco de dados:
 # python manage.py migrate
 # sempre usar para atualizar os models no banco de dados
+
+
+
+
+
+# criando a pasta mde midia 
+# em settings.py adicionar o caminho da pasta midia
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# adicionar o caminho da pasta midia nas urls.py de todos os apps que precise
+# from django.conf import settings
+# from django.conf.urls.static import static
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
