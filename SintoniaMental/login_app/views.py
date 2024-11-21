09b2f311a26ee10c8fa5a557bb5ddc6a141dee9d
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-11-20 22:16:35 -0300

    fazendo back do login

--- SintoniaMental/login_app/views.py
+++ SintoniaMental/login_app/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from cadastro_app.models import Pacientes , Profissionais 
 
-def login(request):
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']   
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None and isinstance(user, Pacientes):
+            login(request, user)
+            return redirect('home_user')
+        else:
+            messages.error(request, 'Email ou senha invalidos !')
+                                                
+    return render(request, 'login.html')
+
+def login_specialist(request):
+    if request.method == 'POST':
+        username = request.POST['username']   
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None and isinstance(user, Profissionais):
+            login(request, user)
+            return redirect('home_specialist')
+        else:
+            messages.error(request, 'Email ou senha invalidos !')
+                                                
     return render(request, 'login.html')
