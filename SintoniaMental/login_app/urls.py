58922d5678ad03799d98241e5c3f4faaadfa3bc7
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2024-11-28 14:11:22 +0000

    modificando a view de login

--- SintoniaMental/login_app/urls.py
+++ SintoniaMental/login_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login_user/', views.login_user , name='login_user'),
-    path('login_specialist/', views.login_specialist , name='login_specialist'),
+    path('login/', views.login , name='login'),
+   
     
    
 ]
--- SintoniaMental/login_app/views.py
+++ SintoniaMental/login_app/views.py
@@ -4,88 +4,45 @@ from django.contrib import messages
 from cadastro_app.models import Pacientes , Profissionais 
 
 
-# def custom_authenticate(name, password):
-#     try:
-#         paciente = Paciente.objects.get(username=name)
-#         if paciente.password == password:
-#             return paciente
-#     except Paciente.DoesNotExist:
-#         pass
+def check_authenticate(name, password):
+    try:
+        paciente = Paciente.objects.get(username=name)
+        if paciente.password == password:
+            return paciente
+    except Paciente.DoesNotExist:
+        pass
 
-#     try:
-#         profissional = Profissional.objects.get(username=name)
-#         if profissional.password == password:
-#             return profissional
-#     except Profissional.DoesNotExist:
-#         pass
+    try:
+        profissional = Profissional.objects.get(username=name)
+        if profissional.password == password:
+            return profissional
+    except Profissional.DoesNotExist:
+        pass
 
-#     return None
-
-# def login_view(request):
-#     if request.method == 'POST':
-#         name = request.POST['name']
-#         password = request.POST['password']
-#         user = custom_authenticate(name, password)
-#         if user is not None and user == paciente:
-#             # Logar o usuário
-#             login(request, user)
-#             return redirect('home')  # Redirecionar para a página inicial
-#         else:
-#             messages.error(request, 'Credenciais inválidas.')
-#     return render(request, 'login.html')
-
-
-# def login_view(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = custom_authenticate(username, password)  # Chama a função de autenticação
-
-#         if user is not None:
-#             # Verifica se o usuário é um Paciente ou um Profissional
-#             if isinstance(user, Paciente):
-#                 # O usuário é um Paciente
-#                 messages.success(request, 'Login como Paciente bem-sucedido.')
-#                 # Aqui você pode redirecionar para uma página específica para Pacientes
-#                 return redirect('home_paciente')  # Exemplo de redirecionamento
-#             elif isinstance(user, Profissional):
-#                 # O usuário é um Profissional
-#                 messages.success(request, 'Login como Profissional bem-sucedido.')
-#                 # Aqui você pode redirecionar para uma página específica para Profissionais
-#                 return redirect('home_profissional')  # Exemplo de redirecionamento
-#         else:
-#             messages.error(request, 'Credenciais inválidas.')  # Mensagem de erro se falhar
-
-#     return render(request, 'login.html')
+    return None
 
 
 
-
-
-
-
-def login_user(request):
+def login(request):
     if request.method == 'POST':
-        username = request.POST['username']   
+        name = request.POST['name']
         password = request.POST['password']
-        user = authenticate(request,username=username, password=password)
-        if user is not None :
-            login(request, user)
-            return redirect('home_user')
+        user = check_authenticate(name, password)  # Chama a função de autenticação
+
+        if user is not None:
+            # Verifica se o usuário esta na tabela Paciente ou Profissional
+            if isinstance(user, Pacientes):
+                messages.success(request, 'Login realizado com sucesso!.')
+                return redirect('home_user')
+            elif isinstance(user, Profissionais):
+                messages.success(request, 'Login como Profissional bem-sucedido.')
+                return redirect('home_specialist') 
         else:
-            messages.error(request, 'Email ou senha invalidos !')
-                                                
+            messages.error(request, 'Email ou senha incorreto !') 
+
     return render(request, 'login.html')
 
-def login_specialist(request):
-    if request.method == 'POST':
-        username = request.POST['username']   
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None and isinstance(user, Profissionais):
-            login(request, user)
-            return redirect('home_specialist')
-        else:
-            messages.error(request, 'Email ou senha invalidos !')
-                                                
-    return render(request, 'login.html')
+
+
+
+
